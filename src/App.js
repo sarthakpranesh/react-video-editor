ed045374d0d0db2ab3b87b19a323571e161d534c
sarthakpranesh <sarthakpranesh08@gmail.com>
2021-12-24 12:52:07 +0530

    Added ffmpeg and used in base file
    Signed-off-by: sarthakpranesh <sarthakpranesh08@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import * as React from "react";
 import './App.css';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
-function App() {
+const App = () => {
+  const [videoPath, setVideoPath] = React.useState('');
+  const [videoSrc, setVideoSrc] = React.useState('');
+  const [message, setMessage] = React.useState('Click Start to transcode');
+  const ffmpeg = createFFmpeg({
+    log: true,
+  });
+
+  const doTranscode = async () => {
+    await ffmpeg.load();
+    setMessage('Start transcoding');
+    ffmpeg.FS('writeFile', 'test.avi', await fetchFile(videoPath)); // need to save the video file to MEMFS before ffmpeg can consume it
+    await ffmpeg.run('-i', 'test.avi', 'test.mp4'); // run the actual operation
+    setMessage('Complete transcoding');
+    const data = ffmpeg.FS('readFile', 'test.mp4');
+    setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p/>
+      <video src={videoSrc} controls></video><br/>
+      <input type="file" onChange={(e) => setVideoPath(e.target.value)} />
+      <button onClick={doTranscode}>Start</button>
+      <p>{message}</p>
     </div>
   );
 }
