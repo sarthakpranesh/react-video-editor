500e7d052aee5023699fdb356b3e711a87398313
sarthakpranesh <sarthakpranesh08@gmail.com>
2021-12-26 13:31:53 +0530

    Using react-ffmpeg and react-use to provide video playback and trimming
    Signed-off-by: sarthakpranesh <sarthakpranesh08@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,34 +1,106 @@
 import * as React from "react";
 import './App.css';
-import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import { useVideo, useMediaDevices } from "react-use";
+import { trimVideo } from "./services";
 
 const App = () => {
-  const [videoPath, setVideoPath] = React.useState('');
+  const [video, state, controls, ref] = useVideo(
+    <video style={styles.video} src="" autoPlay controls />
+  );
+  const [videoFile, setVideoFile] = React.useState(null);
   const [videoSrc, setVideoSrc] = React.useState('');
-  const [message, setMessage] = React.useState('Click Start to transcode');
-  const ffmpeg = createFFmpeg({
-    log: true,
-  });
+  const [trim, setTrim] = React.useState({ start: 0, end: 0 });
 
-  const doTranscode = async () => {
-    await ffmpeg.load();
-    setMessage('Start transcoding');
-    ffmpeg.FS('writeFile', 'test.avi', await fetchFile(videoPath)); // need to save the video file to MEMFS before ffmpeg can consume it
-    await ffmpeg.run('-i', 'test.avi', 'test.mp4'); // run the actual operation
-    setMessage('Complete transcoding');
-    const data = ffmpeg.FS('readFile', 'test.mp4');
-    setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })));
-  };
+  const onTrim = async () => {
+    await trimVideo(videoFile, trim.start, trim.end, function (e) {
+      const video = e.result;
+      setVideoSrc(URL.createObjectURL(video));
+    });
+  }
+
+  const onFileChange = async (e) => {
+    const videoFile = e.target.files[0];
+    setVideoFile(videoFile);
+    setVideoSrc(URL.createObjectURL(videoFile));
+  }
+
+  React.useEffect(() => {
+    if (ref.current === null) {
+      return;
+    }
+    ref.current.src = videoSrc;
+    setTrim({
+      start: 0,
+      end: Math.floor(state.duration),
+    })
+  }, [ref, state.duration, videoSrc]);
 
   return (
-    <div className="App">
-      <p/>
-      <video src={videoSrc} controls></video><br/>
-      <input type="file" onChange={(e) => setVideoPath(e.target.value)} />
-      <button onClick={doTranscode}>Start</button>
-      <p>{message}</p>
+    <div style={styles.mainDivWrapper}>
+      <div>{video}</div>
+      <div style={styles.subDivWrapper}>
+        <span>
+          Select a File: 
+          <input
+            type="file"
+            accept="audio/*,video/*"
+            onChange={onFileChange}
+          />
+        </span>
+        <br />
+        Trim Video Length (time in seconds)
+        <input
+          placeholder="Start time"
+          type="number"
+          value={trim.start}
+          onChange={(e) => {
+            if (e.currentTarget.value < 0) {
+              e.currentTarget.value = 0;
+            }
+            setTrim({
+              start: e.currentTarget.value,
+              end: trim.end,
+            })
+          }}
+        />
+        <input
+          placeholder="End time"
+          type="number"
+          value={trim.end}
+          onChange={(e) => {
+            if (e.currentTarget.value > state.duration) {
+              e.currentTarget.value = state.duration
+            }
+            setTrim({
+              start: trim.start,
+              end: e.currentTarget.value,
+            })
+          }}
+        />
+        <button onClick={onTrim}>Trim</button>
+      </div>
     </div>
   );
 }
+
+const styles = {
+  mainDivWrapper: {
+    height: "100vh",
+    width: "80vw",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  video: {
+    height: "400px",
+    width: "200px",
+    border: "1px solid black",
+  },
+  subDivWrapper: {
+    marginLeft: 20,
+    alignItems: "flex-start",
+  }
+};
 
 export default App;
---
+++ src/services.js
@@ -0,0 +1,28 @@
+import FFMPEG from "react-ffmpeg";
+
+export const convertVideoFromWebmToMp4 = async (file, callback) => {
+    await FFMPEG.process(
+        file,
+        '-fflags +genpts -r 24', //-fflags +genpts -i 1.webm -r 24 1.mp4
+        callback,
+    )
+}
+
+export const ffmpegTimeFormatter = (time) => {
+    // time in seconds
+    const h = Math.floor(time/(60*60)); //seconds * min make hour
+    const m = Math.floor((time - h*60)/60);
+    const s = Math.floor((time - (h*60*60 + m*60)));
+    return `${h}:${m}:${s}`;
+}
+
+export const trimVideo = async (file, start, end, callback) => {
+    const startFormattedTime = ffmpegTimeFormatter(start);
+    const endFormattedTime = ffmpegTimeFormatter(end);
+    console.log(startFormattedTime, endFormattedTime);
+    await FFMPEG.process(
+        file,
+        `-ss ${startFormattedTime} -to ${endFormattedTime} -c:v copy -c:a copy`,
+        callback,
+    )
+}
